68498299971dd0706ade128877207b86d5eadd48
emmaharv <emmharv@gmail.com>
2017-02-16 11:56:43 -0500

    Uploaded AndroidStudio basic app files
    Includes "Setup - Select Reasons" and "Setup - Sentiment Analysis" activities so far.

---
+++ main/java/edu/upenn/cis350/lifestats/SetupActivitySentiment.java
@@ -0,0 +1,13 @@
+package edu.upenn.cis350.lifestats;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetupActivitySentiment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_sentiment);
+    }
+}
