bf715cef30af78987854d414e106fb21784c61a4
kitchena <kitchena@wharton.upenn.edu>
2017-04-20 18:35:52 -0400

    done

--- Project/app/src/main/java/project/cis350/upenn/edu/project/DiaryLogActivity.java
+++ Project/app/src/main/java/project/cis350/upenn/edu/project/DiaryLogActivity.java
@@ -45,89 +45,84 @@ public class DiaryLogActivity extends SideMenuActivity {
 
         username = getIntent().getExtras().getString("username");
     }
+
     public void onSubmitButton(final View v) {
-        new AlertDialog.Builder(this)
-                .setTitle("Submit Diary Entry")
-                .setMessage("Are you sure you want to submit this entry?")
-                //.setIcon(android.R.drawable.ic_dialog_alert)
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
 
-                    public void onClick(DialogInterface dialog, int whichButton) {
-                        Calendar date = Calendar.getInstance();
-                        int month = date.get(Calendar.MONTH);
-                        String d = "";
-                        switch (month) {
-                            case 0:
-                                d += "January ";
-                                break;
-                            case 1:
-                                d += "Febuary ";
-                                break;
-                            case 2:
-                                d += "March ";
-                                break;
-                            case 3:
-                                d += "April ";
-                                break;
-                            case 4:
-                                d += "May ";
-                                break;
-                            case 5:
-                                d += "June ";
-                                break;
-                            case 6:
-                                d += "July ";
-                                break;
-                            case 7:
-                                d += "August ";
-                                break;
-                            case 8:
-                                d += "September ";
-                                break;
-                            case 9:
-                                d += "October ";
-                                break;
-                            case 10:
-                                d += "November ";
-                                break;
-                            case 11:
-                                d += "December ";
-                                break;
+            new AlertDialog.Builder(this)
+                    .setTitle("Submit Diary Entry")
+                    .setMessage("Are you sure you want to submit this entry?")
+                    //.setIcon(android.R.drawable.ic_dialog_alert)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            Calendar date = Calendar.getInstance();
+                            int month = date.get(Calendar.MONTH);
+                            String d = "";
+                            switch (month) {
+                                case 0:
+                                    d += "January ";
+                                    break;
+                                case 1:
+                                    d += "Febuary ";
+                                    break;
+                                case 2:
+                                    d += "March ";
+                                    break;
+                                case 3:
+                                    d += "April ";
+                                    break;
+                                case 4:
+                                    d += "May ";
+                                    break;
+                                case 5:
+                                    d += "June ";
+                                    break;
+                                case 6:
+                                    d += "July ";
+                                    break;
+                                case 7:
+                                    d += "August ";
+                                    break;
+                                case 8:
+                                    d += "September ";
+                                    break;
+                                case 9:
+                                    d += "October ";
+                                    break;
+                                case 10:
+                                    d += "November ";
+                                    break;
+                                case 11:
+                                    d += "December ";
+                                    break;
+                            }
+                            d += date.get(Calendar.DAY_OF_MONTH) + ", " + date.get(Calendar.YEAR);
+
+
+                            //get diary entry
+                            String entry = ((EditText) findViewById(R.id.diary_text)).getText().toString();
+
+                            //get mood from api
+                            Mood m = new Mood(v.getContext());
+                            m.execute(new String[]{entry, username});
+
+
+                            //store entry
+                            DiaryDatabaseHelper dbh = new DiaryDatabaseHelper(v.getContext());
+                            SQLiteDatabase db = dbh.getWritableDatabase();
+                            ContentValues values = new ContentValues();
+                            values.put(DiaryDatabaseContract.DiaryDB.COL_DATE, d + "");
+                            values.put(DiaryDatabaseContract.DiaryDB.COL_ENTRY, entry);
+                            values.put(DiaryDatabaseContract.DiaryDB.COL_USERNAME, username);
+                            db.insert(DiaryDatabaseContract.DiaryDB.TABLE_NAME, null, values);
+                            db.close();
+
+                            AllGoalsActivity.openActivity(DiaryLogActivity.this, username);
+
                         }
-                        d += date.get(Calendar.DAY_OF_MONTH) + ", " + date.get(Calendar.YEAR);
-
-
-
-
-                        //get diary entry
-                        String entry = ((EditText) findViewById(R.id.diary_text)).getText().toString();
-
-                        //get mood from api
-                        Mood m = new Mood(v.getContext());
-                        m.execute(new String[]{entry, username});
-
-
-
-                        //store entry
-                        DiaryDatabaseHelper dbh = new DiaryDatabaseHelper(v.getContext());
-                        SQLiteDatabase db = dbh.getWritableDatabase();
-                        ContentValues values = new ContentValues();
-                        values.put(DiaryDatabaseContract.DiaryDB.COL_DATE, d + "");
-                        values.put(DiaryDatabaseContract.DiaryDB.COL_ENTRY, entry);
-                        values.put(DiaryDatabaseContract.DiaryDB.COL_USERNAME, username);
-                        db.insert(DiaryDatabaseContract.DiaryDB.TABLE_NAME, null, values);
-                        db.close();
-
-                        AllGoalsActivity.openActivity(DiaryLogActivity.this, username);
-                    }})
-                .setNegativeButton(android.R.string.cancel, null).show();
+                    })
+                    .setNegativeButton(android.R.string.cancel, null).show();
 
     }
-   /* public void onMenuButton(View v) {
 
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("username", username);
-        startActivity(intent);
-
-    }*/
 }
